78e334c122d2269dcdf462861f16b45e0a7cbdf6
Justin <justin@garofolo.net>
2019-08-22 01:10:52 -0400

    WebException + NotFound exception inheritance

--- mixer/exceptions.py
+++ mixer/exceptions.py
@@ -1,2 +1,15 @@
-class NotFound(Exception):
-    pass
+class WebException(Exception):
+    """Web request returns unsucessful status."""
+
+    def __init__(self, status, text, msg = None):
+        self.status = status
+        self.text = text
+        msg = msg or "Got status code other than 200."
+        super().__init__(msg)
+
+class NotFound(WebException):
+    """Web request returned 404 status code."""
+
+    def __init__(self, text):
+        msg = "Got status code 404."
+        super().__init__(404, text, msg)
